ce63609da422305dcad98d3c20b5681cb2e093d7
DmitriyLitvin <dimalitvin192.0@gmail.com>
2021-12-16 01:53:25 +0200

    changed host

--- kafka_order/src/main/java/com/example/kafka/KafkaOrderApplication.java
+++ kafka_order/src/main/java/com/example/kafka/KafkaOrderApplication.java
@@ -19,7 +19,7 @@ public class KafkaOrderApplication {
     @Bean
     public KafkaAdmin kafkaAdmin() {
         Map<String, Object> configs = new HashMap<>();
-        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.99.100:9092");
+        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         return new KafkaAdmin(configs);
     }
 }
